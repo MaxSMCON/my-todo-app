4d610202c65977809c43d3c0f48992d5a3234ced
Maxim <smetmx@gmail.com>
2023-06-05 21:37:17 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = 'todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    """read lines from provided file"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+# print(help(get_todos))
+def write_todos(todos_arg, filepath=FILEPATH ):
+    """ Write the to do items list in the text file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ =="__main__":
+    print("Hello")
+    print(get_todos())
